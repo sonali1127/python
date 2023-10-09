386c67c8d7c857c2bd85d7b4e06501c2806a535b
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2023-10-09 21:18:57 +0530

    Add files via upload

---
+++ 9_10_1.py
@@ -0,0 +1,9 @@
+#write a program to print your personal self data
+    #(name,fname,mname,study,habbits,interests,color)
+print('my name is sonali')
+print('my fname is venkata rao')
+print('my mname is pushpa latha')
+print('i am studying BCA')
+print('my habbit is playing games')
+print('my interest is reading books')
+print('my favourite color is black')
