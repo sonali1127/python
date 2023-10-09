00d68d6512378d5515711a709636543bee613284
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2023-10-09 21:19:28 +0530

    Add files via upload

---
+++ 9_10_2.py
@@ -0,0 +1,7 @@
+# write a program to print all the arithmetic operations
+a=int(input("enter a value: "))
+b=int(input("enter b value: "))
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
