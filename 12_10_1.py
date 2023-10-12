e358c9208361d735b94af617cea89ef50abf5821
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2023-10-12 21:13:25 +0530

    Add files via upload

---
+++ 12_10_1.py
@@ -0,0 +1,6 @@
+'''Ass1:
+Write a program to print given number is greater than 50 or not.
+If it is greater than 50,print " hi"
+Otherwise print "bye"'''
+a=int(input("enter a value:"))
+print("hi") if a>50 else print("bye")
