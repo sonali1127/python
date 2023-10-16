cbca64c279789e2d5a46ed5534a4b0f763e9be68
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2023-10-16 19:06:43 +0530

    Add files via upload

---
+++ 16_10_3.py
@@ -0,0 +1,8 @@
+'''ass3:
+    write program to print the following series
+    5   10   15   20   25  30 ------n'''
+n=int(input("enter a n value:"))
+i=5
+while(i<=n):
+    print(i,end=" ")
+    i=i+5
