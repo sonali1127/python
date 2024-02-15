06fde8b3c55ef7e9b5c9013d56f6f30a31b37b1b
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2024-02-15 20:51:57 +0530

    Add files via upload

---
+++ 15_2_3.py
@@ -0,0 +1,7 @@
+#write a program to print given two values are equal or not
+a=int(input("Enter A value:"))
+b=int(input("Enter B value:"))
+if(a==b):
+    print("Two numbers are equal")
+else:
+    print("Two numbers are not equal")
