596e9e8653af108bb61a93bc0d4e6212d49457b0
sonali1127 <125128826+sonali1127@users.noreply.github.com>
2024-02-15 20:52:13 +0530

    Add files via upload

---
+++ 15_2_4.py
@@ -0,0 +1,6 @@
+#write a program to print given year is leap year or not
+y=int(input("Enter a year:"))
+if((y%4==0 and y%100!=0) or y%400==0):
+    print("Leap year")
+else:
+    print("Not a Leap year")
